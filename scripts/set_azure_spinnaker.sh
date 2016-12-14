d541089329e167663e7110dc79ac6bb930d60e60
Damien Caro <dcaro@microsoft.com>
2016-12-14 13:39:48 -0800

    Addin the params

--- scripts/set_azure_spinnaker.sh
+++ scripts/set_azure_spinnaker.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-# Get Azure Resources Names 
-SERVICEPRINCIPALNAME=$1
-SUBSCRIPTIONID=$2
-OBJECTID=$3
-CLIENTID=$4
-TENANTID=$5
+# Get azure data 
+# clientId : -c 
+# AppKey: -a 
 
-# Create a test file 
+
+while getopts ":t:s:p:c:" opt; do
+  case $opt in
+    t) TENANTID="$OPTARG"
+    ;;
+    p) PASSWORD="$OPTARG"
+    ;;
+    c) CLIENTID="$OPTARG"
+    ;;
+    s) SUBSCRIPTIONID="$OPTARG"
+  esac
+done
+
+# Record the Variables for debugging  
 sudo touch /tmp/helloworld
-echo $SERVICEPRINCIPALNAME >> /tmp/helloworld 
+sudo printf "Tenant: %s \n" $TENANTID >> /tmp/helloworld
+sudo printf "Secret is %s \n" $PASSWORD >> /tmp/helloworld
+sudo printf "Client ID %s \n" $CLIENTID >> /tmp/helloworld
+sudo printf "Subscription is %s \n" $SUBSCRIPTIONID >> /tmp/helloworld
+
+# Install Spinnaker on the VM
+sudo bash -xc "$(curl -s https://raw.githubusercontent.com/spinnaker/spinnaker/master/InstallSpinnaker.sh)"
+
 
