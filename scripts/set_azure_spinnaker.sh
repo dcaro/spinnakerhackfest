5ae3f1f2aee4447bf62f22fec463e619926a504d
Damien Caro <dcaro@microsoft.com>
2016-12-14 11:57:01 -0800

    Adding shell var

--- scripts/set_azure_spinnaker.sh
+++ scripts/set_azure_spinnaker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Get Azure Resources Names 
+SERVICEPRINCIPALNAME=$1
+SUBSCRIPTIONID=$2
+OBJECTID=$3
+CLIENTID=$4
+TENANTID=$5
+
 # Create a test file 
-touch /tmp/helloworld
+sudo touch /tmp/helloworld
+echo $SERVICEPRINCIPALNAME >> /tmp/helloworld 
+
