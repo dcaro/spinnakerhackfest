3fafb8c5adf752c6b88352a4e6fe2bca9f7bb270
Damien Caro <dcaro@microsoft.com>
2017-01-18 16:46:03 -0800

    Groovy

--- setup-scripts/init_jenkins.sh
+++ setup-scripts/init_jenkins.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# This script to configure jenkins automatically
 
+# This script to configure the following stuff from Jenkins automatically: JDK, Oracle user and password, Gradle 
+java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://jenkins:Passw0rd@localhost:8080 groovy setup_jenkins.groovy 
+
+# Create the job from the XML template 
+java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://jenkins:Passw0rd@localhost:8080 create-job MyJob < jenkins_job.xml
--- setup-scripts/setup_jenkins.groovy
+++ setup-scripts/setup_jenkins.groovy
@@ -7,18 +7,23 @@ import com.cloudbees.plugins.credentials.impl.*;
 import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
 
+import hudson.tasks.Gradle
 
 def descriptor = new JDK.DescriptorImpl();
+def OracleUser = args[0];
+def OraclePwd = args[1];
+def GitHubUser = args[2];
+def GitHubPwd = args[3];
 
 // Add github credentials to Jenkins domains
 println 'create github user credentials'
-Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"github_user", "description", "user", "password")
+Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"github_user", "GitHub user", GitHubUser, GitHubPwd)
 SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
 
 // Add Orcale user credentials for JDK
 def inst = Jenkins.getInstance()
 def desc = inst.getDescriptor("hudson.tools.JDKInstaller")
-println desc.doPostCredential('mickey@disney.com','mypassword')
+println desc.doPostCredential(OracleUser,OraclePwd)
 
 // Add the JDK installation  
 if (descriptor.getInstallations()) {
@@ -32,10 +37,12 @@ if (descriptor.getInstallations()) {
 }
 
 // Add the Gradle configuration
-
+// def gradle = new Gradle.GradleInstallation('gradle_3.3', null, [new InstallSourceProperty([new Gradle.GradleInstaller('3.3')])])
 
 
 // Create the pipeline
 // To be done with Jenkins CLI 
 //  java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://user:password@localhost:8080 get-job "Build Hello World" > jenkins_job.xml
 
+// java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://jenkins:Passw0rd@localhost:8080 groovy setup_jenkins.groovy
+// java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://jenkins:Passw0rd@localhost:8080 create-job MyJob < jenkins_job.xml
