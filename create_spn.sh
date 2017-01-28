17e4170ba44358ce06008e1e4f443e08da231cad
Damien Caro <dcaro@microsoft.com>
2017-01-27 18:50:22 -0800

    with SPN

--- create_spn.sh
+++ create_spn.sh
@@ -56,21 +56,22 @@ echo "Application Uri: "$APPLICATION_URI
 echo "Application Key: "$APPLICATION_KEY
 
 # Obtain the tenantId of the subscriptions
-if [ -z "$TENANT_ID" ] 
+if [ -z "$TENANT_ID" ]
 then
     AZURE_ACCOUNT=$(az account show --subscription "$SUBSCRIPTION_NAME")
     echo $AZURE_ACCOUNT
     TENANT_ID=$(echo $AZURE_ACCOUNT | jq .tenantId | sed 's/"//g')
-    echo $TENANT_ID
+    CLOUD_ID=$(echo $AZURE_ACCOUNT | jq .id | sed 's/"//g')
+    echo "TenantId = $TENANT_ID"
+    echo "CloudId = $CLOUD_ID"
 fi 
 
 
-# Create the application
-
+### Create the application
 # Check if the application already exist
 my_error_check=$(az ad app list --filter "identifierUris/any(identifierUris: identifierUris eq '$APPLICATION_URI')" | jq '. | length')
 
-if [ $my_error_check > 0 ];
+if [[ $my_error_check > 0 ]] ;
 then
     echo "An application already exist with this identifer-uri: $APPLICATION_URI"
     echo "We will use that application"
@@ -84,25 +85,25 @@ else
     ###### Add the code to verify if the app has been created 
 fi
 
-    #APP_ID=$(echo $AZAD_APP | jq .appId | sed 's/"//g')
-    APP_ID=$(az ad app list --filter "identifierUris/any(identifierUris: identifierUris eq 'http://$APPLICATION_URI')" | jq -r '.[0].appId')
-    echo "Application ID is: $APP_ID"
-    if [ $APP_ID = '' ];
+### Create the SPN 
+# Obtain the ApplicationID 
+APP_ID=$(az ad app list --filter "identifierUris/any(identifierUris: identifierUris eq 'http://$APPLICATION_URI')" | jq -r '.[0].appId')
+echo "Application ID is: $APP_ID"
+if [ -z $APP_ID ];
+then 
+    echo "Application not created"
+else
+    # The application exist, we can create the SPN
+    error_check=$(az ad sp list --filter "servicePrincipalNames/any(servicePrincipalNames: servicePrincipalNames eq 'http://$APPLICATION_URI')")
+    if [ -z $error_check ];
     then 
-        echo "Application not created"
-    else
-        # We can create the SPN 
-        error_check=$(az ad sp list --filter "servicePrincipalNames/any(servicePrincipalNames: servicePrincipalNames eq 'http://$APPLICATION_URI')")
-        if [ $error_check -gt 0 ];
-        then 
-            SPN=$(az ad sp create --id="$APP_ID")
-            echo $SPN
-        fi
+        SPN=$(az ad sp create --id="$APP_ID")
+        # | jq -r '.[0].objectId'
+        echo "SPN is $SPN"
+        ######## Need to get the SPNID here
     fi
-
-
-
+fi
 
 # Do the role assignment 
 
-az role assignment create --assignee="ae1ff666-4390-425d-a198-d5c96fe67830" --role="Owner" --scope="/subscriptions/0c3a2f71-4128-4509-8719-3b16f291ad5f"
+#az role assignment create --assignee="$SPN_ObjectID" --role="Owner" --scope="/subscriptions/$CLOUD_ID"
