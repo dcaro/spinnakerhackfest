b2fccef518523bd040dbe0db5fc19ba38abf6647
Damien Caro <dcaro@microsoft.com>
2017-01-27 18:06:07 -0800

    update spn script

--- create_spn.sh
+++ create_spn.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Add jq
+# sudo apt-get install jq -y 
+
 # Default values 
 app_uuid=$(python -c 'import uuid; print str(uuid.uuid4())[:8]')
 DISPLAY_NAME="spinnaker"
 APPLICATION_NAME="app${app_uuid}"
-APPLICATION_URI="{$APPLICATION_NAME}_id"
+APPLICATION_URI="${APPLICATION_NAME}_id"
 APPLICATION_KEY=$(python -c 'import uuid; print uuid.uuid4().hex')
-
+TENANT_ID=""
+SUBSCRIPTION_NAME="Dev Ops_events_dcaro"
 
 while [[ $# -gt 1 ]]
 do
@@ -26,7 +30,15 @@ case $key in
    shift
    ;;
    -p)
-   SPN_PASSWORD="$2"
+   APPLICATION_KEY="$2"
+   shift
+   ;;
+   -t)
+   TENANT_ID="$2"
+   shift
+   ;;
+   -n)
+   SUBSCRIPTION_NAME="$2"
    shift
    ;;
    *)
@@ -36,27 +48,61 @@ esac
 shift
 done
 
-echo $DISPLAY_NAME
-echo $APPLICATION_NAME
-echo $APPLICATION_URI
-echo $APPLICATION_KEY
+echo "Debug information:"
+echo "Tenant Id: "$TENANT_ID
+echo "Display Name: "$DISPLAY_NAME
+echo "Application Name: "$APPLICATION_NAME
+echo "Application Uri: "$APPLICATION_URI
+echo "Application Key: "$APPLICATION_KEY
 
-
-my_error_check=$(az ad sp show --search $DISPLAY_NAME --json | grep "displayName" | grep -c \"$DISPLAY_NAME\")
-# Check if the application already exists
-if [ $my_error_check -gt 0 ];
+# Obtain the tenantId of the subscriptions
+if [ -z "$TENANT_ID" ] 
 then
-    echo "error application exist"
-else
-    # Create the application
-    my_app=$(az ad app create --display-name=$DISPLAY_NAME --homepage=$APPLICATION_NAME --identifier-uris=$APPLICATION_URI --key-type="Password" --password=$APPLICATION_KEY)
+    AZURE_ACCOUNT=$(az account show --subscription "$SUBSCRIPTION_NAME")
+    echo $AZURE_ACCOUNT
+    TENANT_ID=$(echo $AZURE_ACCOUNT | jq .tenantId | sed 's/"//g')
+    echo $TENANT_ID
+fi 
 
+
+# Create the application
+
+# Check if the application already exist
+my_error_check=$(az ad app list --filter "identifierUris/any(identifierUris: identifierUris eq '$APPLICATION_URI')" | jq '. | length')
+
+if [ $my_error_check > 0 ];
+then
+    echo "An application already exist with this identifer-uri: $APPLICATION_URI"
+    echo "We will use that application"
+else
+    # Create the application 
+    AZAD_APP=$(az ad app create --display-name="$DISPLAY_NAME" --homepage="http://$APPLICATION_NAME" --identifier-uris="http://$APPLICATION_URI" --key-type="Password" --password="$APPLICATION_KEY")
+    echo $AZAD_APP
+    echo "Waiting for the creation of the app"
+    sleep 5 
+    #  Verify if the application has been created
+    ###### Add the code to verify if the app has been created 
 fi
 
-echo $my_app | jq .appId | sed 's/"//g'
+    #APP_ID=$(echo $AZAD_APP | jq .appId | sed 's/"//g')
+    APP_ID=$(az ad app list --filter "identifierUris/any(identifierUris: identifierUris eq 'http://$APPLICATION_URI')" | jq -r '.[0].appId')
+    echo "Application ID is: $APP_ID"
+    if [ $APP_ID = '' ];
+    then 
+        echo "Application not created"
+    else
+        # We can create the SPN 
+        error_check=$(az ad sp list --filter "servicePrincipalNames/any(servicePrincipalNames: servicePrincipalNames eq 'http://$APPLICATION_URI')")
+        if [ $error_check -gt 0 ];
+        then 
+            SPN=$(az ad sp create --id="$APP_ID")
+            echo $SPN
+        fi
+    fi
 
-# Create the service principal attached
-az ad sp create --id= ""
+
+
 
 # Do the role assignment 
-#az role assignment create --assignee="ae1ff666-4390-425d-a198-d5c96fe67830" --role="Owner" --scope="/subscriptions/0c3a2f71-4128-4509-8719-3b16f291ad5f"
+
+az role assignment create --assignee="ae1ff666-4390-425d-a198-d5c96fe67830" --role="Owner" --scope="/subscriptions/0c3a2f71-4128-4509-8719-3b16f291ad5f"
